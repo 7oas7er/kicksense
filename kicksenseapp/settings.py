033a31736dc8c6dbda2cb0c1c56e8ddc99b37cf5
Frank Schlesinger <Frank.Schlesinger@immobilienscout24.de>
2016-01-13 21:33:44 +0100

    tweaking around with static files dir

--- kicksenseapp/settings.py
+++ kicksenseapp/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
