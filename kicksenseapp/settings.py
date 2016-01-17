78ee1b293e8cb76fdce7bdb99f82d1dffa458c5c
7oas7er <Frank@Schlesinger.com>
2016-01-17 19:40:58 +0100

    FIX: Trying to fix issue with static files dir

--- kicksenseapp/settings.py
+++ kicksenseapp/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_URL = '/kicksenseapp/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
