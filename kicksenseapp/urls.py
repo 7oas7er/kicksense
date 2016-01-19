015c2d98cb8c2e60fc6d2e0d09668cc92811de33
7oas7er <Frank@Schlesinger.com>
2016-01-19 15:12:24 +0100

    FIX: Try to solve delivery of static files on heroku

--- kicksenseapp/urls.py
+++ kicksenseapp/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from collector import views
 from monitor.views import MoveeventList
+import settings
 
 urlpatterns = [
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^admin', include(admin.site.urls)),
     url(r'^moveevent/$', views.moveevent_list),
     url(r'^moveevent/(?P<pk>[0-9]+)/$', views.moveevent_detail),
