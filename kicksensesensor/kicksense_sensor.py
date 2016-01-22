de41e85b3218e0ed46765ad1250cde183a82ea59
Gurkenmilch <sebastian.nachtigall@googlemail.com>
2016-01-22 20:53:46 +0000

    LED is working now with nice pictures

--- kicksensesensor/kicksense_sensor.py
+++ kicksensesensor/kicksense_sensor.py
@@ -1,5 +1,5 @@
-# from sense_hat import SenseHat
-import http.client as httplib, sys, time, math, json
+from sense_hat import SenseHat
+import http.client as httplib, sys, time, math, json, threading, random
 
 baseUri = 'https://stormy-woodland-9591.herokuapp.com/moveevent'
 id="kicker1"
@@ -11,6 +11,25 @@ headers = {}
 headers['Content-Type'] = 'application/json'
 headers['User-Agent'] = 'Kicker Rasperry Pi'
 headers['Accept'] = '*/*'
+
+
+
+def crab_blink():
+    sense.load_image("crab4.png")
+    time.sleep(0.1)
+    sense.load_image("crab1.png")
+
+class MyThread ( threading.Thread ):
+
+   def run ( self ):
+       while True:
+           crab_blink()
+           time.sleep(random.randint(3,6))
+           
+
+      
+
+MyThread().start()
 
 def createConnection():
     conn = httplib.HTTPConnection('stormy-woodland-9591.herokuapp.com')
@@ -31,14 +50,47 @@ def changed(x,y,z):
     print ("Current Delta Z> ", math.fabs(z-z1))
 
     return math.fabs(x-x1)>rumbleOffset or math.fabs(y-y1)>rumbleOffset or math.fabs(z-z1)>rumbleOffset
+def alligator():
+    sense.load_image("aligator2.png")
+    time.sleep(0.1)
+    sense.load_image("aligator3.png")
+    time.sleep(0.2)
+    sense.load_image("aligator2.png")
+    time.sleep(0.1)
+    sense.load_image("aligator3.png")
+    time.sleep(0.2)
+    sense.load_image("aligator2.png")
+    time.sleep(0.1)
+    sense.load_image("aligator3.png")
+    time.sleep(0.2)
+    sense.load_image("aligator2.png")
+    time.sleep(0.1)
+    sense.load_image("aligator3.png")
+
+
+def crab():
+    sense.load_image("crab1.png")
+    time.sleep(0.1)
+    sense.load_image("crab2.png")
+    time.sleep(0.3)
+    sense.load_image("crab1.png")
+    time.sleep(0.1)
+    sense.load_image("crab2.png")
+    time.sleep(0.3)
+    sense.load_image("crab1.png")
+    time.sleep(0.1)
+    sense.load_image("crab2.png")
+    time.sleep(0.2)
+    sense.load_image("crab1.png")
+    time.sleep(0.1)
+
 
 
 conn = createConnection()
 while True:
     x, y, z = getValues()
-
     print(x,y,z)
-    time.sleep(1)
+    time.sleep(0.5)
     if changed(x,y,z):
         print ("Changed!!")
         body={"x":x,"y":y,"z":z}
@@ -52,13 +104,14 @@ while True:
             print ("POST is raus, Antwort lautet")
             reply = conn.getresponse()
             print (reply.read())
-            # sense.show_message("!", text_colour=[255, 0, 0])
-            sense.load_image("circle.png")
-            # sense.set_pixels()
+            crab()
+            
+           
 
         except:
             e = sys.exc_info()[0]
             print('Oh, Request could not be sent: {}', e)
             conn = createConnection()
+
 
 
